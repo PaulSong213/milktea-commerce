c7263579705691414d7be6311d2cfd3eb26a1e31
Mojakoness <113253409+chiyoness@users.noreply.github.com>
2023-08-10 11:30:22 +0800

    a

--- inventory/productTable.php
+++ inventory/productTable.php
@@ -35,6 +35,10 @@
 
 <body>
     <div class="table w-100">
+        <div class="xp-menubar bg-danger" >
+            <span class="material-icons text-white">signal_cellular_alt
+            </span>
+        </div>
         <h2 class="mt-4 mb-5">PHARMACY INVENTORY</h2>
 
         <?php include 'add.php'; // Include the modal content ?>
@@ -48,6 +52,7 @@
                     <th>Age</th>
                     <th>Start date</th>
                     <th>Salary</th>
+                    <th>Action</th>
 
                 </tr>
             </thead>
@@ -59,8 +64,9 @@
                     <td>61</td>
                     <td>2011-04-25</td>
                     <td>$320,800</td>
+                    <td>NO</td>
                 </tr>
-               
+
             </tbody>
         </table>
     </div>
@@ -76,9 +82,9 @@
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables-buttons/2.2.0/js/buttons.colVis.js"></script>
 
-    
+
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
 
             $('#example thead tr')
                 .clone(true)
@@ -92,43 +98,43 @@
                 autoFill: true,
                 dom: 'Bfrtip',
                 buttons: [{
-                        extend: 'excelHtml5',
-                        className: 'btn btn-success'
-                    },
-                    {
-                        extend: 'pdfHtml5',
-                        className: 'btn btn-primary'
-                    },
-                    {
-                        extend: 'print',
-                        className: 'btn border border-info'
-                    },
-                    {
-                        extend: 'colvis',
-                        className: 'btn border border-info'
-                    },
-                    {
-                        extend: 'pageLength',
-                        className: 'btn btn-primary'
-                    },
-                    {
-                        text: 'Add Item',
-                        className: 'btn btn-primary bg-primary text-white',
-                        action: function(e, dt, node, config) {
-                            $('#addItemModal').modal('show'); 
-                        }
+                    extend: 'excelHtml5',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    className: 'btn btn-primary'
+                },
+                {
+                    extend: 'print',
+                    className: 'btn border border-info'
+                },
+                {
+                    extend: 'colvis',
+                    className: 'btn border border-info'
+                },
+                {
+                    extend: 'pageLength',
+                    className: 'btn btn-primary'
+                },
+                {
+                    text: 'Add Item',
+                    className: 'btn btn-primary bg-primary text-white',
+                    action: function (e, dt, node, config) {
+                        $('#addItemModal').modal('show');
                     }
+                }
                 ],
 
-                
-                initComplete: function() {
+
+                initComplete: function () {
                     var api = this.api();
 
                     // For each column
                     api
                         .columns()
                         .eq(0)
-                        .each(function(colIdx) {
+                        .each(function (colIdx) {
                             // Set the header cell to contain the input element
                             var cell = $('.filters th').eq(
                                 $(api.column(colIdx).header()).index()
@@ -139,11 +145,11 @@
 
                             // On every keypress in this input
                             $(
-                                    'input',
-                                    $('.filters th').eq($(api.column(colIdx).header()).index())
-                                )
+                                'input',
+                                $('.filters th').eq($(api.column(colIdx).header()).index())
+                            )
                                 .off('keyup change')
-                                .on('change', function(e) {
+                                .on('change', function (e) {
                                     // Get the search value
                                     $(this).attr('title', $(this).val());
                                     var regexr =
@@ -155,15 +161,15 @@
                                         .column(colIdx)
                                         .search(
                                             this.value != '' ?
-                                            regexr.replace('{search}', '(((' + this.value +
-                                                ')))') :
-                                            '',
+                                                regexr.replace('{search}', '(((' + this.value +
+                                                    ')))') :
+                                                '',
                                             this.value != '',
                                             this.value == ''
                                         )
                                         .draw();
                                 })
-                                .on('keyup', function(e) {
+                                .on('keyup', function (e) {
                                     e.stopPropagation();
 
                                     $(this).trigger('change');
@@ -177,18 +183,34 @@
         });
     </script>
 
-<script>
-   $('#saveItemButton').click(function() {
+    <script>
+        $('#saveItemButton').click(function () {
             $('#addItemModal').modal('hide'); // Close the modal after saving
         });
 
-        $('#Closemodal2').click(function() {
+        $('#Closemodal2').click(function () {
             $('#addItemModal').modal('hide'); // Close the modal when the close button is clicked
         });
-        $('#Closemodal1').click(function() {
+        $('#Closemodal1').click(function () {
             $('#addItemModal').modal('hide'); // Close the modal when the close button is clicked
         });
-</script>
+    </script>
+
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+            $(".xp-menubar").on('click', function () {
+                $('#sidebar').toggleClass('active');
+                $('#content').toggleClass('active');
+            });
+
+            $(".xp-menubar,.body-overlay").on('click', function () {
+                $('#sidebar,.body-overlay').toggleClass('show-nav');
+            });
+
+        });
+
+    </script>
 </body>
 
 </html>
