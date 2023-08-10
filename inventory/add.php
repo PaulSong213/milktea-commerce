fc0433c15710e94f384a4c6574e0b7638cb2b395
Mojakoness <113253409+chiyoness@users.noreply.github.com>
2023-08-10 11:26:09 +0800

    a

--- inventory/add.php
+++ inventory/add.php
@@ -1,10 +1,11 @@
 <!-- modal_content.php -->
-<div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="addItemModalLabel" aria-hidden="true">
+<div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="addItemModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="addItemModalLabel">Add Item</h5>
-                <button type="button" class="close"  id="Closemodal1" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" id="Closemodal1" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -12,26 +13,27 @@
                 <!-- Your modal content goes here -->
                 <label for="item_code">Item Code:</label>
                 <input type="text" id="item_code" class="form-control" placeholder="Enter item code">
-                
+
                 <label for="type">Type</label>
-				<select class="form-control" id="type" name="type">
-					<option value="type1">Type 1</option>
-					<option value="type2">Type 2</option>
-					<option value="type3">Type 3</option>
-				</select>
+                <select class="form-control" id="type" name="type">
+                    <option value="type1">Type 1</option>
+                    <option value="type2">Type 2</option>
+                    <option value="type3">Type 3</option>
+                </select>
 
                 <label for="Unit">Unit:</label>
                 <input type="number" id="Unit" class="form-control" placeholder="Enter Unit">
 
                 <label for="description">Description</label>
-				<textarea class="form-control" id="description" name="description" placeholder="Enter description"></textarea>
-		
+                <textarea class="form-control" id="description" name="description"
+                    placeholder="Enter description"></textarea>
+
                 <label for="Generic">Generic:</label>
                 <input type="text" id="Generic" class="form-control" placeholder="Enter Generic">
 
                 <label for="Sugprice">Sug Price:</label>
                 <input type="number" id="Sugprice" class="form-control" placeholder="0.0000">
-                
+
                 <label for="MWprice">MW Price:</label>
                 <input type="number" id="MWprice" class="form-control" placeholder="0.00">
 
@@ -42,7 +44,7 @@
                 <input type="number" id="Ppriceuse" class="form-control" placeholder="0.0000">
 
 
-                
+
                 <!-- Add more fields as needed -->
             </div>
             <div class="modal-footer">
