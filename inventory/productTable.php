e8b7c80ffbc43d6b70056ade47c350bdeafda7ca
Ryan Josua Sargento <ryanjoshua.sargeto@cvsu.edu.ph>
2023-08-11 10:03:37 +0800

    database

--- inventory/productTable.php
+++ inventory/productTable.php
@@ -40,7 +40,7 @@
         <table id="example" class="table table-striped" style="width:100%">
             <thead>
                 <tr>
-
+                    <th>Date</th>
                     <th>Item Code</th>
                     <th>Unit</th>
                     <th>Unit Type</th>
@@ -70,7 +70,7 @@
                     $statusColor = ($row["Status"]  == "1") ? "bg-success"  : "bg-danger"; //condition for color bg.
                     echo "
                                 <tr>
-                                   
+                                    <td>" . $row["createDate"] . "</td>
                                     <td>" . $row["itemCode"] . "</td>
                                     <td>" . $row["Unit"] . "</td>
                                     <td>" . $row["Type"] . "</td>
