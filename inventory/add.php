4512fd8b85d12bef16cebff80fa390ce95c265e1
Mojakoness <113253409+chiyoness@users.noreply.github.com>
2023-08-11 11:42:22 +0800

    a

--- inventory/add.php
+++ inventory/add.php
@@ -4,35 +4,6 @@
 
 <head>
     <title>Suggested Input Text</title>
-    <style>
-        /* Some basic styling */
-        #container {
-            position: relative;
-        }
-
-        #suggestions {
-            position: absolute;
-            border: 1px solid #ccc;
-            max-height: 150px;
-            overflow-y: auto;
-            opacity: 0;
-            transition: opacity 0.2s ease-in-out;
-            width: 100%;
-            /* Match the input width */
-            background-color: white;
-            /* To match input background */
-            border-top: none;
-            /* To remove border overlap */
-            border-bottom-left-radius: 4px;
-            /* Rounded corners */
-            border-bottom-right-radius: 4px;
-        }
-
-        /* Add a class to show suggestions */
-        #suggestions.active {
-            opacity: 1;
-        }
-    </style>
 </head>
 
 <body>
@@ -52,39 +23,36 @@
                         <!-- Your modal content goes here -->
                         <label for="item_code">Item Code:</label>
                         <input type="text" id="item_code" name="item_code" class="form-control"
-                            placeholder="Enter item code" required>
-
-                        <div id="container">
-                            <label for="type">Type</label>
-                            <input type="text" class="form-control" id="type" name="type" placeholder="Input Type" autocomplete="on">
-                        </div>
+                            placeholder="Enter item code" required autocomplete="on">
+                        <label for="type">Type</label>
+                        <input type="text" class="form-control" id="type" name="type" placeholder="Input Type"
+                            autocomplete="on">
                         <label for="Unit">Unit:</label>
-                        <input type="number" id="Unit" class="form-control" name="Unit" placeholder="0"
-                            required>
+                        <input type="number" id="Unit" class="form-control" name="Unit" placeholder="0" required>
 
                         <label for="description">Description</label>
                         <textarea class="form-control" id="description" name="description"
-                            placeholder="Enter description" required></textarea>
+                            placeholder="Enter description" required autocomplete="on"></textarea>
 
                         <label for="Generic">Generic:</label>
                         <input type="text" id="Generic" class="form-control" name="Generic" placeholder="Enter Generic"
-                            required>
+                            required autocomplete="on">
 
                         <label for="Sugprice">Sug Price:</label>
                         <input type="number" id="Sugprice" class="form-control" name="Sugprice" placeholder="0.0000"
-                            required>
+                            required autocomplete="on">
 
                         <label for="MWprice">MW Price:</label>
                         <input type="number" id="MWprice" class="form-control" name="MWprice" placeholder="0.00"
-                            required>
+                            required autocomplete="on">
 
                         <label for="IPDprice">IPD Percent:</label>
                         <input type="number" id="IPDprice" class="form-control" name="IPDprice" placeholder="0.00 %"
-                            required>
+                            required autocomplete="on">
 
                         <label for="Ppriceuse">Ppriceuse:</label>
                         <input type="number" id="Ppriceuse" class="form-control" name="Ppriceuse" placeholder="0.0000"
-                            required>
+                            required autocomplete="on">
                         <!-- Add more fields as needed -->
                     </div>
                     <div class="modal-footer">
