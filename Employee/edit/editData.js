f9a382492a786fa0895ba0eca6617fa2c21e5aa7
John Paul Songalia <songaliajohnpaul@gmail.com>
2023-08-17 12:11:11 +0800

    Add modal message for employee actions

--- Employee/edit/editData.js
+++ Employee/edit/editData.js
@@ -1,65 +1,68 @@
 export function handleEditClick(addModalLocator) {
     $(".edit-btn").on('click', function (e) {
-        
+
         const addModal = $("#addItemModal");
         addModal.modal('show');
         let data = JSON.parse($(this).attr("data-item"));
         console.log(data);
 
         const toFillUpDatas = [
-              {
+            {
                 dataKey: "lname",
                 inputName: "employee_lname"
-                },
-                {
+            },
+            {
                 dataKey: "fname",
                 inputName: "employee_fname"
-                },
-                 {
+            },
+            {
                 dataKey: "mname",
                 inputName: "employee_mname"
-                },
-                {
+            },
+            {
                 dataKey: "nickName",
                 inputName: "employee_nickname"
-                },
-                 {
+            },
+            {
                 dataKey: "bDate",
                 inputName: "employee_bdate"
-                },
-                  {
-                dataKey: "maritalStatus	",
+            },
+            {
+                dataKey: "maritalStatus",
                 inputName: "marital"
-                },
-                 {
+            },
+            {
                 dataKey: "sex",
                 inputName: "sex"
-                },
-                {
+            },
+            {
                 dataKey: "department",
                 inputName: "dept"
-                },                
-                {
+            },
+            {
                 dataKey: "title",
                 inputName: "title"
-                },
-                 {
+            },
+            {
                 dataKey: "position",
                 inputName: "position"
-                },  
-                 {
+            },
+            {
                 dataKey: "dateStart",
                 inputName: "employee_sdate"
-                }, 
-                {
+            },
+            {
                 dataKey: "userName",
                 inputName: "email"
-                },   
-                {
+            },
+            {
                 dataKey: "password",
                 inputName: "Password"
-                },
-            
+            },
+            {
+                dataKey: "password",
+                inputName: "CPassword"
+            },
         ];
 
         // fill up the fields
@@ -69,7 +72,7 @@ export function handleEditClick(addModalLocator) {
         }
 
         // edit the save button
-        const saveButton = $("[name='SaveChanges']");
+        const saveButton = $("[name='SaveItem']");
         saveButton.text("Edit Employee Information");
 
         // edit header title
@@ -78,13 +81,14 @@ export function handleEditClick(addModalLocator) {
 
         const addItemForm = $("#addItemForm");
         addItemForm.attr("action", "./edit/editfunction.php");
-        addItemForm.append(`<input type="text" name="item_id" value="${data['DatabaseID']}">`);
+        addItemForm.append(`<input type="hidden" name="item_id" value="${data['DatabaseID']}">`);
 
         // watch modal close then reset data
         addModal.on("hidden.bs.modal", function () {
             headerTitle.text("Add Item");
             saveButton.text("Add Item");
             addItemForm.attr("action", "addfunction.php");
+            addItemForm.find("input[name='item_id']").remove();
             for (let i = 0; i < toFillUpDatas.length; i++) {
                 const toFillUpData = toFillUpDatas[i];
                 $(`[name="${toFillUpData.inputName}"]`).val("");
