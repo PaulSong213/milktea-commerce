4fc6e16d950714ab0b9461685c5339da9c58fc6b
Ryan Josua Sargento <ryanjoshua.sargeto@cvsu.edu.ph>
2023-08-21 15:36:34 +0800

    patientTb

--- Patient/table.php
+++ Patient/table.php
@@ -58,7 +58,7 @@
                     <th>Gender</th>
                     <th>Date Added</th>
                     <th>Modified Date</th>
-                    <th>Actions</th>
+                    <th class="action-column">Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -128,15 +128,24 @@
                 dom: 'Bfrtip',
                 buttons: [{
                         extend: 'excelHtml5',
-                        className: 'btn btn-success'
+                        className: 'btn border border-info',
+                        exportOptions: {
+                            columns: ':not(.action-column)'
+                        }
                     },
                     {
                         extend: 'pdfHtml5',
-                        className: 'btn btn-primary'
+                        className: 'btn border border-info',
+                        exportOptions: {
+                            columns: ':not(.action-column)'
+                        }
                     },
                     {
                         extend: 'print',
-                        className: 'btn border border-info'
+                        className: 'btn border border-info',
+                        exportOptions: {
+                            columns: ':not(.action-column)'
+                        }
                     },
                     {
                         extend: 'colvis',
@@ -154,6 +163,9 @@
                         }
                     }
                 ],
+
+
+
                 initComplete: function() {
                     searchColumn(this.api());
                 },
