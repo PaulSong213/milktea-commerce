cb86d0ba3afdd62f15771975d95df215fc8f6ae5
Mojakoness <113253409+chiyoness@users.noreply.github.com>
2023-08-29 16:12:12 +0800

    a

--- billing_slip/chargeslipIpd.php
+++ billing_slip/chargeslipIpd.php
@@ -367,7 +367,7 @@ $LastBillingID = getLastBillingID($conn);
             var datalist = document.getElementById('product_id_list');
 
             var xhr = new XMLHttpRequest();
-            xhr.open("GET", "get_product_details.php?itemCode=" + selectedValue, true);
+            xhr.open("GET", "getproductdetails.php?itemCode=" + selectedValue, true);
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     if (xhr.status === 200) {
@@ -549,5 +549,4 @@ $LastBillingID = getLastBillingID($conn);
         });
     </script>
 </body>
-
 </html>
