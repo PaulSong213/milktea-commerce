466080e52faa5d63378e41f015c04e089f9ccba7
Ryan Josua Sargento <ryanjoshua.sargeto@cvsu.edu.ph>
2023-09-08 16:39:18 +0800

    add backlog

--- departmentTable/edit/editfunction.php
+++ departmentTable/edit/editfunction.php
@@ -3,6 +3,12 @@ require_once '../../php/connect.php';
 session_start();
 $conn = connect();
 
+if (isset($_SESSION['user'])) {
+    $userData = json_decode($_SESSION['user'], true);
+    $userID = $userData['DatabaseID'];
+    $userDepartment = $userData['departmentName'];
+}
+
 if (isset($_POST['SaveItem'])) {
     $itemTypeID = $_POST['item_id'];
     $departmentName = $_POST['departmentName'];
@@ -19,6 +25,14 @@ if (isset($_POST['SaveItem'])) {
 
     $result = mysqli_query($conn, $sql);
     if ($result) {
+
+        $act = "Edit Department Data";
+        $description = "Edit Department Data";
+
+        $conn1 = connect();
+        $sql1 = "INSERT INTO backlog_tb (employeeID, action, description, timeStamp)
+        						VALUES ('$userID', '$act', '$description', NOW())";
+        $result1 = mysqli_query($conn1, $sql1);
         // success
         $_SESSION["alert_message"] = "Successfully Edited an Department";
         $_SESSION["alert_message_success"] = true;
