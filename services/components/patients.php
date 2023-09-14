adcf2b3d5f4c18654308b84a7357b3de50fca29d
John Paul Songalia <songaliajohnpaul@gmail.com>
2023-09-14 16:05:40 +0800

    Add patient logic

--- services/components/patients.php
+++ services/components/patients.php
@@ -2,8 +2,8 @@
 <div id="patientContainer" class="rounded p-3 my-2 text-left border border-white">
     <div class="row mb-1">
         <div class="col-6">
-            <label for="hospitalRecordNo" class="form-label">Hospital #</label>
-            <input type="text" placeholder="Enter Patient Hospital No." class="form-select border border-secondary" id="hospitalRecordNo" list="patientList" correctData="patientsData" required>
+            <label for="patientHospitalRecordNo" class="form-label">Hospital #</label>
+            <input type="text" placeholder="Enter Patient Hospital No." class="form-select border border-secondary" id="patientHospitalRecordNo" name="patientHospitalRecordNo" list="patientList" correctData="patientsData" required autocomplete="off">
             <?php require_once('../API/datalist/patient-list.php') ?>
             <small id="patientRecordIndicator" class="text-success fw-bold d-none">PATIENT HAS PREVIOUS RECORD</small>
         </div>
@@ -11,32 +11,32 @@
     <div class="row mb-1">
         <div class="col">
             <label for="patientLastname" class="form-label">Last Name</label>
-            <input type="text" placeholder="Enter Patient Last name" class="form-control border border-secondary" id="patientLastname" required>
+            <input type="text" placeholder="Enter Patient Last name" class="form-control border border-secondary" id="patientLastname" name="patientLastname" required>
         </div>
         <div class="col">
             <label for="patientFirstname" class="form-label">First Name</label>
-            <input type="text" placeholder="Enter Patient First Name" class="form-control border border-secondary" id="patientFirstname" required>
+            <input type="text" placeholder="Enter Patient First Name" class="form-control border border-secondary" id="patientFirstname" name="patientFirstname" required>
         </div>
 
     </div>
     <div class="row mb-1">
         <div class="col">
             <label for="patientMiddlename" class="form-label">Middle Name</label>
-            <input type="text" placeholder="Enter Patient Middle Name" class="form-control border border-secondary" id="patientMiddlename">
+            <input type="text" placeholder="Enter Patient Middle Name" class="form-control border border-secondary" name="patientMiddlename" id="patientMiddlename">
         </div>
         <div class="col">
             <label for="patientBdate" class="form-label">Birth Date</label>
-            <input type="date" class="form-control border border-secondary" id="patientBdate">
+            <input type="date" class="form-control border border-secondary" id="patientBdate" name="patientBdate">
         </div>
     </div>
     <div class="row mb-1">
         <div class="col">
             <label for="patientAge" class="form-label">Age</label>
-            <input type="text" placeholder="Enter Patient Age" class="form-control border border-secondary" id="patientAge">
+            <input type="text" placeholder="Enter Patient Age" class="form-control border border-secondary" id="patientAge" name="patientAge">
         </div>
         <div class="col">
             <label for="patientGender" class="form-label">Sex</label>
-            <select class="form-select" id="patientGender" name="patientGender">
+            <select class="form-select" id="patientGender" name="patientGender" name="patientGender">
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
             </select>
@@ -51,13 +51,13 @@
 
         const patientsData = JSON.parse('<?php echo $patientsData; ?>');
 
-        $("#hospitalRecordNo").on('input', function() {
+        $("#patientHospitalRecordNo").on('input', function() {
             const recordVal = $(this).val();
-            const hospitalRecordNo = extractID(recordVal);
+            const patientHospitalRecordNo = extractID(recordVal);
             $("#patientContainer input").val('');
-            $(this).val(hospitalRecordNo ? hospitalRecordNo : recordVal);
-            if (!patientsData[hospitalRecordNo]) return;
-            const currentPatient = patientsData[hospitalRecordNo].data;
+            $(this).val(patientHospitalRecordNo ? patientHospitalRecordNo : recordVal);
+            if (!patientsData[patientHospitalRecordNo]) return;
+            const currentPatient = patientsData[patientHospitalRecordNo].data;
             console.log(currentPatient);
             $("#patientLastname").val(currentPatient.lname);
             $("#patientFirstname").val(currentPatient.fname);
--- services/process.php
+++ services/process.php
@@ -12,14 +12,42 @@ if (isset($_POST['SaveItem'])) {
     // Retrieve form inputs
     $selectedServices = $_POST['services_table'];
     $RequestedName = $_POST['doctor_name'];
-    $PatientName = $_POST['patient_name'];
+    $patientHospitalRecordNo = $_POST['patientHospitalRecordNo'];
     $remarks = $_POST['additional_info'];
 
     $lastTwoDigits = substr($RequestedName, -2);
 
+    // Check if the patient already exists
+    if ($patientHospitalRecordNo) {
+        die();
+        $checkQuery = "SELECT hospitalRecordNo FROM patient_tb WHERE hospitalRecordNo = '$patientHospitalRecordNo'";
+        $checkResult = mysqli_query($conn, $checkQuery);
+        if (mysqli_num_rows($checkResult) <= 0) { // patient has no record
+            // insert the new patient
+            $patientLastname = $_POST['patientLastname'];
+            $patientFirstname = $_POST['patientFirstname'];
+            $patientMiddlename = $_POST['patientMiddlename'];
+            $patientBdate = $_POST['patientBdate'];
+            $patientAge = $_POST['patientAge'];
+            $patientGender = $_POST['patientGender'];
+            $insertPatientQuery = "INSERT INTO `patient_tb` (`hospistalrecordNo`, `lname`, `fname`, `mname`, `age`, `gender`, `bDate`) VALUES ('$patientHospitalRecordNo','$patientLastname','$patientLastname','$patientMiddlename','$patientAge','$patientGender');";
+            $insertPatientResult = mysqli_query($conn, $insertPatientQuery);
+
+            $resultInsertPatient = mysqli_query($conn, $insertPatientQuery);
+            if (!$resultInsertPatient) {
+                $_SESSION["alert_message"] = "Failed to insert data into the database. Error Details: " . mysqli_error($conn);
+                $_SESSION["alert_message_error"] = true;
+                header("Location: ./index.php");
+                die();
+            }
+        }
+    }
+
+
+
     // Create the INSERT query
     $insertQuery = "INSERT INTO services_tb ( Services, RequestedName, PatientName, remarks)
-                    VALUES ( '$selectedServices', '$lastTwoDigits', '$PatientName', '$remarks')";
+                    VALUES ( '$selectedServices', '$lastTwoDigits', '$patientHospitalRecordNo', '$remarks')";
 
     // Execute the query
     $result = mysqli_query($conn, $insertQuery);
@@ -43,4 +71,3 @@ if (isset($_POST['SaveItem'])) {
 
 // Close the database connection
 $conn->close();
-?>
