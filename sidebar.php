fcaa8d2cf0aa19a88017ce83187bd8b0bb55a65d
John Paul Songalia <songaliajohnpaul@gmail.com>
2023-09-25 14:26:19 +0800

    Fixed sidebar

--- sidebar.php
+++ sidebar.php
@@ -25,343 +25,6 @@ if (isset($_GET['logout'])) {
 
 // get icons here -> https://mui.com/material-ui/material-icons/
 
-
-$LeveL1 = [
-    [
-        "name" => "Patient", //name of the link
-        "icon" => "hotel", //material icon name
-        "link" => "/Patient/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Enter new Patient Record", //name of the  sub-link
-                "link" => "/Patient/index.php", //link of the page
-            ],
-            [
-                "name" => "Enter Billing (New Admission)", //name of the linksub-link
-                "link" => "/billing-new-admission/index.php", //link of the page
-            ],
-        ] //list of links on dropdown
-    ],
-    [
-        "name" => $userFullName, //name of the link
-        "icon" => "account_circle", //material icon name
-        "link" => "/dashboard/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => isset($_SESSION['user']) ? json_decode($_SESSION['user'], true)['userName'] : 'You are Logout',
-                "icon" => "account_circle", //material icon name
-                "link" => "/dashboard/index.php", //link of the pages
-            ],
-            [
-                "name" => "Log Out", //name of the link
-                "link" => "./logout.php", //link of the page
-            ],
-
-        ] //list of links on dropdown
-    ],
-];
-$Level2 = [
-    [
-        "name" => "Dashboard", //name of the link
-        "icon" => "dashboard", //material icon name
-        "link" => "/dashboard/index.php", //link of the page
-        "navigations" => [] //list of links on dropdown
-    ],
-
-    [
-        "name" => "Employee", //name of the link
-        "icon" => "badge", //material icon name
-        "link" => "/Employee/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Employee", //name of the link
-                "link" => "/Employee/index.php", //link of the page
-            ],
-            [
-                "name" => "Department", //name of the link
-                "link" => "/departmentTable/index.php", //link of the page
-            ],
-            [
-                "name" => "Room", //name of the link
-                "link" => "/Room/index.php", //link of the page
-            ],
-
-        ] //list of links on dropdown
-    ],
-    [
-        "name" => "Patient", //name of the link
-        "icon" => "hotel", //material icon name
-        "link" => "/Patient/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Enter new Patient Record", //name of the link
-                "link" => "/Patient/index.php", //link of the page
-            ],
-            [
-                "name" => "Enter Billing (New Admission)", //name of the link
-                "link" => "/billing-new-admission/index.php", //link of the page
-            ],
-        ] //list of links on dropdown
-    ],
-
-    [
-        "name" => $userFullName, //name of the link
-        "icon" => "account_circle", //material icon name
-        "link" => "/dashboard/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => isset($_SESSION['user']) ? json_decode($_SESSION['user'], true)['userName'] : 'You are Logout',
-                "icon" => "account_circle", //material icon name
-                "link" => "/dashboard/index.php", //link of the pages
-            ],
-            [
-                "name" => "Log Out", //name of the link
-                "link" => "./logout.php", //link of the page
-            ],
-
-        ] //list of links on dropdown
-    ],
-];
-$LeveL3 = [
-    [
-        "name" => "Dashboard", //name of the link
-        "icon" => "dashboard", //material icon name
-        "link" => "/dashboard/index.php", //link of the page
-        "navigations" => [] //list of links on dropdown
-    ],
-
-    [
-        "name" => "Charge Slip", //name of the link
-        "icon" => "point_of_sale", //material icon name
-        "link" => "/billing_slip/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "ChargeBilling/IPD/OPD", //name of the link
-                "link" => "/billing_slip/index.php", //link of the page
-            ],
-            [
-                "name" => "Billing List", //name of the link
-                "link" => "/billinglist/index.php", //link of the page
-            ],
-            [
-                "name" => "Clinic Use", //name of the link
-                "link" => "/clinicUse/index.php", //link of the page
-            ],
-            [
-                "name" => "Print Closing Report", // include returns the included content
-                "link" => "/closingReport/index.php",
-                "id" => "openModalButton",
-            ],
-            // [
-            //     "name" => "Print Montly Report", // include returns the included content
-            //     "link" => "/MontlyReport/index.php",
-            //     "id" => "openModalButton",
-            // ],
-        ]
-    ],
-    [
-        "name" => "Patient", //name of the link
-        "icon" => "hotel", //material icon name
-        "link" => "/Patient/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Enter new Patient Record", //name of the link
-                "link" => "/Patient/index.php", //link of the page
-            ],
-            [
-                "name" => "Enter Billing (New Admission)", //name of the link
-                "link" => "/billing-new-admission/index.php", //link of the page
-            ],
-        ] //list of links on dropdown
-    ],
-    [
-        "name" => $userFullName, //name of the link
-        "icon" => "account_circle", //material icon name
-        "link" => "/dashboard/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => isset($_SESSION['user']) ? json_decode($_SESSION['user'], true)['userName'] : 'You are Logout',
-                "icon" => "account_circle", //material icon name
-                "link" => "/dashboard/index.php", //link of the pages
-            ],
-            [
-                "name" => "Log Out", //name of the link
-                "link" => "./logout.php", //link of the page
-            ],
-
-        ] //list of links on dropdown
-    ],
-];
-$LeveL4 = [
-    [
-        "name" => "Dashboard", //name of the link
-        "icon" => "dashboard", //material icon name
-        "link" => "/dashboard/index.php", //link of the page
-        "navigations" => [] //list of links on dropdown
-    ],
-    [
-        "name" => "Charge Slip", //name of the link
-        "icon" => "point_of_sale", //material icon name
-        "link" => "/billing_slip/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "ChargeBilling/IPD/OPD", //name of the link
-                "link" => "/billing_slip/index.php", //link of the page
-            ],
-            [
-                "name" => "Charge List", //name of the link
-                "link" => "/ChargeTable/index.php", //link of the page
-            ],
-
-            [
-                "name" => "Billing List", //name of the link
-                "link" => "/billinglist/index.php", //link of the page
-            ],
-            [
-                "name" => "Clinic Use", //name of the link
-                "link" => "/clinicUse/index.php", //link of the page
-            ],
-            [
-                "name" => "Clinic Use Table", //name of the link
-                "link" => "/clinicUsetable/index.php", //link of the page
-            ],
-            [
-                "name" => "Print Closing Report", // include returns the included content
-                "link" => "/closingReport/index.php",
-            ],
-            // [
-            //     "name" => "Print Montly Report", // include returns the included content
-            //     "link" => "/MontlyReport/index.php",
-            //     "id" => "openModalButton",
-            // ],
-        ]
-    ],
-    [
-        "name" => "Enter Payment", //name of the link
-        "icon" => "paid", //material icon name
-        "link" => "/enterPayment/index.php?type=cash", //link of the page
-        "navigations" => [
-            [
-                "name" => "Enter Payment", //name of the link
-                "link" => "/enterPayment/index.php?type=cash", //link of the page
-            ],
-            [
-                "name" => "Payment History", //name of the link
-                "link" => "/enterPayment/history/index.php", //link of the page
-            ]
-        ]
-    ],
-    [
-        "name" => "Employee", //name of the link
-        "icon" => "badge", //material icon name
-        "link" => "/Employee/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Employee", //name of the link
-                "link" => "/Employee/index.php", //link of the page
-            ],
-            [
-                "name" => "Department", //name of the link
-                "link" => "/departmentTable/index.php", //link of the page
-            ],
-            [
-                "name" => "Room", //name of the link
-                "link" => "/Room/index.php", //link of the page
-            ],
-
-        ] //list of links on dropdown
-    ],
-    [
-        "name" => "Patient", //name of the link
-        "icon" => "hotel", //material icon name
-        "link" => "/Patient/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Enter new Patient Record", //name of the link
-                "link" => "/Patient/index.php", //link of the page
-            ],
-            [
-                "name" => "Enter Billing (New Admission)", //name of the link
-                "link" => "/billing-new-admission/index.php", //link of the page
-            ],
-        ] //list of links on dropdown
-    ],
-    [
-        "name" => "Inventory", //name of the link
-        "icon" => "vaccines", //material icon name
-        "link" => "/inventory/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => "Inventory Items", //name of the link
-                "link" => "/inventory/index.php", //link of the page
-            ],
-            [
-                "name" => "Item Types", //name of the link
-                "link" => "/itemType/index.php", //link of the page
-            ],
-            [
-                "name" => "Supplier", //name of the link
-                "icon" => "local_shipping", //material icon name
-                "link" => "/SupplierTable/index.php", //link of the page
-                "navigations" => [] //list of links on dropdown
-            ],
-            [
-                "name" => "Expenses", //name of the link
-                "link" => "/Expenses/index.php", //link of the page
-                "navigations" => [] //list of links on dropdown
-            ],
-        ] //list of links on dropdown
-    ],
-    [
-        "name" => "Back Logs", //name of the link
-        "icon" => "history", //material icon name
-        "link" => "/backlog/index.php", //link of the page
-        "navigations" => [] //list of links on dropdown
-    ],
-    [
-        "name" => $userFullName, //name of the link
-        "icon" => "account_circle", //material icon name
-        "link" => "/account/index.php", //link of the page
-        "navigations" => [
-            [
-                "name" => isset($_SESSION['user']) ? json_decode($_SESSION['user'], true)['userName'] : 'You are Logout',
-                "icon" => "account_circle", //material icon name
-                "link" => "/account/index.php", //link of the pages
-            ],
-            [
-                "name" => isset($_SESSION['user']) ? json_decode($_SESSION['user'], true)['departmentName'] : 'You are Logout',
-                "icon" => "account_circle", //material icon name
-                "link" => "/account/index.php", //link of the pages
-            ],
-            [
-                "name" => "Log Out", //name of the link
-                "link" => "./logout.php", //link of the page
-            ],
-
-        ] //list of links on dropdown
-    ],
-];
-$LevelNav = ""; // Define $LevelNav with an initial value
-
-switch ($userLevel) {
-    case 1:
-        $LevelNav = $LeveL1;
-        break;
-    case 2:
-        $LevelNav = $LeveL2;
-        break;
-    case 3:
-        $LevelNav = $LeveL3;
-        break;
-    case 4:
-    case ($Level >= 4):
-        $LevelNav = $LeveL4;
-        break;
-    default:
-        // Handle other cases if necessary
-        break;
-}
-
 require_once(__DIR__ . "/accessLevels.php");
 
 
@@ -387,7 +50,7 @@ require_once(__DIR__ . "/accessLevels.php");
             width: 200px;
             height: 100vh;
             transition: ease-in 0.1s;
-            background-color: #fff;
+            background-color: #010409;
             box-shadow: 0 0 30px rgba(200, 200, 200, 0.2);
             z-index: 1000;
         }
@@ -416,7 +79,7 @@ require_once(__DIR__ . "/accessLevels.php");
 
         .sidebar-header {
             padding: 20px;
-            background-color: #fff;
+            background-color: #000000;
             border-bottom: 1px solid #eee;
         }
 
@@ -449,19 +112,20 @@ require_once(__DIR__ . "/accessLevels.php");
 
         .list-unstyled.components li a {
             padding: 10px 20px;
-            color: #777;
+            color: #fff;
             display: block;
             transition: background-color 0.3s, color 0.3s;
+            border-radius: 7px;
         }
 
         .list-unstyled.components li.active a {
-            background-color: #dbe5fd;
-            color: #4c7cf3;
+            background-color: #804428;
+            color: #fff;
         }
 
         .list-unstyled.components li a:hover {
-            background-color: #dbe5fd;
-            color: #4c7cf3;
+            background-color: #804428;
+            color: #fff;
         }
 
         .dropdown-toggle::after {
@@ -531,10 +195,13 @@ require_once(__DIR__ . "/accessLevels.php");
 
 <body style="height: 100%; overflow-y: auto;">
     <div style="height: 100%; overflow-y: auto;">
-        <nav id="sidebar" class="bg-light" style="height: 100%; overflow-y: auto;">
-            <a href="../account/index.php">
+        <nav class="rounded" id="sidebar" style="height: 100%; overflow-y: auto;">
+            <a class="text-decoration-none" href="../account/index.php">
                 <div class="sidebar-header">
-                    <h3><img src="/milktea-commerce/img/logo1.png" class="img-fluid" alt="Logo" /><span class="fw-bold company-title">ROMEO'S CAFE</span></h3>
+                    <h3>
+                        <img src="/milktea-commerce/img/logo1.png" class="img-fluid" alt="Logo" />
+                        <span class="fw-bold company-title text-decoration-none text-white fs-6">ROMEO'S CAFE</span>
+                    </h3>
                 </div>
             </a>
             <ul class="list-unstyled components">
